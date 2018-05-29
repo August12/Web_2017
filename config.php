d8353b45d3cde1683166cfca913384190bcb447f
JungJun Hwang <jungjunhwang@JungJunui-MacBook-Air.local>
2018-05-29 14:27:47 +0900

    revise

--- config.php
+++ config.php
@@ -5,7 +5,6 @@
 		//$query_line = 'select * from user_table';
 		try{
 			$db = new PDO("mysql:dbname=".$db_name."", "root", "root");
-			echo "sex";
 			//$lines = $db->query($query_line);
 		}catch (PDOException $ex) {
 			echo "Error";
